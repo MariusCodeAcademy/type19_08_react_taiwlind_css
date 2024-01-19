0f521feb588f4ed5ef974b4e389d2735125c0185
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-19 11:44:30 +0200

    our team

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import './styles/App.css';
+import TeamSection from './components/TeamSection';
 
 export default function App() {
   return (
-    <div className='App container mx-auto'>
-      <h1 className='text-3xl font-bold underline'>Hello, world!</h1>
-      <button className='px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm'>
-        Nice button
-      </button>
-      <h2>Hello, world!</h2>
-      <p>Hello, world!</p>
-      <hr />
+    <div className='App '>
+      <TeamSection />
     </div>
   );
 }
