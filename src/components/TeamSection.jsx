93a7488446500360191ca75ca3e2f0e7a5c7de0b
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-22 08:48:02 +0200

    4 els

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -1,4 +1,12 @@
 export default function TeamSection() {
+  const newArr = [];
+
+  for (let i = 1; i < 5; i++) {
+    newArr.push(i);
+  }
+
+  console.log('newArr ===', newArr);
+
   return (
     <section className='py-16 border-b'>
       <div className='container px-12 mx-auto'>
@@ -10,32 +18,29 @@ export default function TeamSection() {
 
         <ul className='pt-10 grid lg:grid-cols-3 md:grid-cols-2 gap-6 '>
           {/* When i hover on li element i want to add 'translate-x' rule to ul el inside, how to do that in  */}
-          <li className='border-slate-700 border-2 group'>
-            <div className='relative overflow-hidden'>
-              <img src='/img/p1.webp' alt='guy' />
-              <ul className='bg-blue-500 -translate-x-full text-white absolute top-0 left-0 h-full group-hover:translate-x-0 transition-transform'>
-                <li className='p-4'>icon1</li>
-                <li className='p-4'>icon2</li>
-              </ul>
-            </div>
-            <div>
-              <h3>Name</h3>
-              <p>Pareigos</p>
-            </div>
-          </li>
-          <li className=' border-slate-700 border-2'>
-            <div className='relative'>
-              <img src='/img/p1.webp' alt='guy' />
-              <ul>
-                <li>icon1</li>
-                <li>icon2</li>
-              </ul>
-            </div>
-            <div>
-              <h3>Name</h3>
-              <p>Pareigos</p>
-            </div>
-          </li>
+          {newArr.map((sk) => (
+            <li key={sk} className='border-slate-700 border-2 group'>
+              <div className='relative overflow-hidden'>
+                <img className='w-full' src='/img/p1.webp' alt='guy' />
+                <ul className='bg-blue-500 -translate-x-full text-white absolute top-0 left-0 h-full group-hover:translate-x-0 transition-transform'>
+                  <li className='p-4 hover:bg-blue-400'>
+                    <a href='#'>
+                      <span className='lnr text-4xl lnr-apartment'></span>
+                    </a>
+                  </li>
+                  <li className='p-4 hover:bg-blue-400'>
+                    <a href='#'>
+                      <span className='lnr text-4xl lnr-dice'></span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className='py-4 px-6'>
+                <h3 className='text-2xl font-bold'>Name</h3>
+                <p className='italic'>Pareigos</p>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
